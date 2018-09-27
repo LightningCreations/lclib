5bdd7575634a8af155369fc7a02b86b0534559eb
InfernoDeity <chorman64@gmail.com>
2018-09-27 11:44:30 -0400

    Update Hash.hpp

--- include/lclib-cxx/Hash.hpp
+++ include/lclib-cxx/Hash.hpp
@@ -10,6 +10,8 @@
 #include <utility>
 #include <map>
 #include <array>
+#include <variant>
+#include <lclib-cxx/Vector.hpp>
 
 
 class LIBLCAPI Hashable{
@@ -58,19 +60,25 @@ constexpr int32_t hashcode(double d){
 	}u = {d};
 	return hashcode(u.bits);
 }
-constexpr int32_t hashcode(void* v){
+constexpr int32_t hashcode(const void* v){
 	return int32_t(v);
 }
 constexpr int32_t hashcode(nullptr_t n){
 	return 0;
 }
-LIBLCAPI int32_t hashcode(const char*);
+constexpr int32_t hashcode(const char* str){
+	int32_t h = 0;
+	while(*str!=0)
+		(h*=31,h+=hashcode(*str),str++);
+	return h;
+}
+
 LIBLCAPI int32_t hashcode(const std::string&);
 LIBLCAPI int32_t hashcode(const std::type_info&);
 
 using std::vector;
 
-template<typename T> int32_t hashcode(const vector<T>& v){
+template<typename T> auto hashcode(const vector<T>& v)->decltype(hashcode(v[0])){
 	int32_t h = 0;
 	for(int i = 0;i<v.size();i++){
 		h*=31;
@@ -81,7 +89,7 @@ template<typename T> int32_t hashcode(const vector<T>& v){
 
 using std::array;
 
-template<typename T,size_t size>  int32_t hashcode(const array<T,size>& a){
+template<typename T,size_t size>  auto hashcode(const array<T,size>& a)->decltype(hashcode(a[0])){
 	int32_t h = 0;
 	for(int i = 0;i<size;i++){
 		h*=31;
@@ -90,7 +98,7 @@ template<typename T,size_t size>  int32_t hashcode(const array<T,size>& a){
 	return h;
 } 
 
-template<typename T,size_t size> constexpr int32_t hashcode(const T(&a)[size]){
+template<typename T,size_t size> constexpr auto hashcode(const T(&a)[size])->decltype(hashcode(a[0])){
 	int32_t h = 0;
 	for(const T& val:a){
 		h*=31;
@@ -105,27 +113,17 @@ using std::shared_ptr;
 const int32_t uniqueSmartPrime = 11443717;
 const int32_t sharedSmartPrime = 540283;
 
-template<typename T> int32_t hashcode(const unique_ptr<T>&ptr){
+template<typename T> auto hashcode(const unique_ptr<T>&ptr)->decltype(hashcode(ptr.get())){
 	return hashcode(ptr.get())*uniqueSmartPrime;
 }
 
-template<typename T> int32_t hashcode(const shared_ptr<T>& ptr){
+template<typename T> auto hashcode(const shared_ptr<T>& ptr)->decltype(hashcode(ptr.get())){
 	return hashcode(ptr.get())*sharedSmartPrime;
 }
 
 using std::pair;
-template<typename T,typename U> int32_t hashcode(const pair<T,U>& p){
+template<typename T,typename U> auto hashcode(const pair<T,U>& p)->std::common_type_t<decltype(hashcode(p.first)),decltype(hashcode(p.second))>{
 	return hashcode(p.first)*31+hashcode(p.second);
-}
-
-template<typename T> int32_t hashcode(const pair<T*,size_t>& a){
-	//Treat as array not as pair
-	int32_t h = 0;
-	for(int i =0;i<a.second();i++){
-		h*=31;
-		h+=hashcode(a.first()[i]);
-	}
-	return h;
 }
 
 using std::map;
@@ -138,13 +136,39 @@ template<typename K,typename V> int32_t hashcode(const map<K,V>& m){
 	}
 	return hash;
 }
+template<typename... T> constexpr auto hashcode(const std::variant<T...>& var)->std::common_type_t<decltype(hashcode(std::declval<T>()))...>{
+	return std::visit([](auto&& a)->std::common_type_t<decltype(hashcode(std::declval<T>()))...>{return hashcode(std::forward(a));},var);
+}
+
+
+
+namespace detail{
+	template<typename T> constexpr int32_t hashcodes(T&& t){
+		return hashcode(t);
+	}
+	constexpr int32_t hashcodes(){
+		return 0;
+	}
+	template<typename T,typename... Ts> constexpr int32_t hashcodes(T&& t,Ts&&... valList){
+		return hashcode(std::forward(t))*31+hashcodes(std::forward(valList)...);
+	}
+	template<typename Tuple,std::size_t... Ids> constexpr auto hashcode(const Tuple& t,std::index_sequence<Ids...>)
+		->std::common_type_t<decltype(hashcode(std::get<Ids>(t)))...>{
+		return hashcodes(std::get<Ids>(t)...);
+	}
+}
+
+template<typename Tuple,typename=std::void_t<
+			decltype(detail::hashcode(std::declval<const Tuple&>(),std::make_index_sequence<std::tuple_size_v<Tuple>>{}))
+		>
+	>
+	constexpr decltype(auto) hashcode(const Tuple& t){
+	return detail::hashcode(t,std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+}
+
 
 template<typename E> constexpr int32_t hashcode(typename std::enable_if<std::is_enum<E>::value,E>::type e){
-	union{
-		E e1;
-		typename std::underlying_type<E>::type bits;
-	}u = {e};
-	return hashcode(u.bits);
+	return hashcode(static_cast<std::underlying_type_t<E>>(e));
 }
 
 

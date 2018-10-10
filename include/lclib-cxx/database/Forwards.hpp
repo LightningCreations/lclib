c40325445a10bc02db3b0c4a12ffa451d56ca4e1
InfernoDeity <chorman64@gmail.com>
2018-10-10 16:46:30 -0400

    Add files via upload

---
+++ include/lclib-cxx/database/Forwards.hpp
@@ -0,0 +1,20 @@
+/*
+ * Forwards.hpp
+ *
+ *  Created on: Oct 10, 2018
+ *      Author: connor
+ */
+
+#ifndef __INCLUDE_LCLIB_CXX_DATABASE_FORWARDS_HPP__2018_10_10_16_05_51
+#define __INCLUDE_LCLIB_CXX_DATABASE_FORWARDS_HPP__2018_10_10_16_05_51
+
+namespace db{
+	class Connection;
+	class Statement;
+	class Rowset;
+	class PreparedStatement;
+}
+
+
+
+#endif /* __INCLUDE_LCLIB_CXX_DATABASE_FORWARDS_HPP__2018_10_10_16_05_51 */

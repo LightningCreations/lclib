3045c9c7f24f6423df52f837b2d0a731200aac6f
connor <connor@connor-Aspire-ES1-533>
2018-12-21 09:59:21 -0500

    Fix potential race condition in URE. Man I am terrible with using atomics

--- src/UniqueRandomEngine.cpp
+++ src/UniqueRandomEngine.cpp
@@ -6,14 +6,14 @@
  */
 
 #include <lclib/UniqueRandomEngine.hpp>
+#include <atomic>
 
 uint64_t detail::seedGenHelper(uint64_t clSalt){
 	const auto tp = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
 	const static uint64_t cprime{182854975377317};
-	static uint64_t prime{242102480888143};
-	uint64_t ret{((prime*cprime)>>16)+tp.time_since_epoch().count()*clSalt};
-	prime *= cprime;
-	prime++;
-	return ret;
+	static std::atomic<uint64_t> prime{242102480888143};
+	uint64_t val{prime.load()};
+	while(prime.compare_exchange_strong(val,val*cprime+1));
+	return val*tp+clSalt;
 }
 
